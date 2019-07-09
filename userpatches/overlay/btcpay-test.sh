80877e1d16ceb4b781f16da998e455fd0c6df3a4
nicolas.dorier <nicolas.dorier@gmail.com>
2019-07-09 15:55:33 +0900

    Add BTCPayServer build

---
+++ userpatches/overlay/btcpay-test.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+set -e
+
+source /opt/btcpay/btcpay-common.sh
+
+if [ -f /sys/devices/platform/leds/leds/diy-led/brightness ]; then
+    red_led=/sys/devices/platform/leds/leds/diy-led/brightness
+    white_led=/sys/devices/platform/leds/leds/work-led/brightness
+elif [ -f /sys/devices/platform/leds/leds/standby-led/brightness ]; then
+    red_led=/sys/devices/platform/leds/leds/standby-led/brightness
+    white_led=/sys/devices/platform/leds/leds/power-led/brightness
+else
+    echo "Impossible to find the white and red leds"
+    exit 1
+fi
+
+echo '255' > $white_led
+echo '255' > $red_led
+
+white_led_value=255
+drive_mounted=false
+docker_running=false
+container_started=false
+btcpay_init_exited=false
+btcpayserver_pinged=false
+fancontrol_running=false
+
+blink_speed=1
+timeout=$(((60*15)))
+
+total_wait=0
+success=false
+while true; do
+    echo "$white_led_value" > $white_led
+    if ! $drive_mounted && [ "$(systemctl show -p SubState --value mnt-external.mount)" == "mounted" ]; then
+        drive_mounted=true
+        echo -e "[ \e[32mOK\e[0m ] The external drive is correctly mounted"
+    fi
+
+    if ! $docker_running && [ "$(systemctl show -p SubState --value docker.service)" == "running" ]; then
+        docker_running=true
+        echo -e "[ \e[32mOK\e[0m ] Docker started"
+    fi
+
+    if ! $container_started && [ "$(docker ps -aq -f status=running -f name=generated_btcpayserver_1)" ]; then
+        container_started=true
+        echo -e "[ \e[32mOK\e[0m ] BTCPayServer container is started"
+    fi
+
+    if ! $btcpayserver_pinged && [ "$(curl -sL -w "%{http_code}\\n" "http://localhost/" -o /dev/null)" == "200" ]; then
+        btcpayserver_pinged=true
+        echo -e "[ \e[32mOK\e[0m ] BTCPayServer is online"
+    fi
+
+    if ! $btcpay_init_exited && [ "$(systemctl show -p SubState --value btcpay-init)" == "dead" ]; then
+        btcpay_init_exited=true
+        echo -e "[ \e[32mOK\e[0m ] BTCPay-Init exited"
+    fi
+
+    if ! $fancontrol_running && [ "$(systemctl show -p SubState --value fancontrol.service)" == "running" ]; then
+        fancontrol_running=true
+        echo -e "[ \e[32mOK\e[0m ] The Fan control service is running"
+    elif $fancontrol_running && [ "$(systemctl show -p SubState --value fancontrol.service)" != "running" ]; then
+        fancontrol_running=false
+        echo -e "[ \e[31mFailed\e[0m ] The Fan control service is not running"
+    fi
+
+    if $drive_mounted && $docker_running && $container_started && $btcpayserver_pinged && $fancontrol_running && $btcpay_init_exited; then
+        echo -e "[ \e[32mOK\e[0m ] All tests passed"
+        success=true
+        break
+    elif [ $total_wait -ge $timeout ]; then
+        success=false
+        break
+    fi
+
+    white_led_value=$(((white_led_value==0?255:0)))
+    sleep $blink_speed
+    total_wait=$(((total_wait+blink_speed)))
+done
+
+if $success; then
+    if [ -f "/root/.need-factory-setup" ]; then
+        echo "0" > $white_led
+        rm -rf /root/.not_logged_in_yet
+        rm -rf /root/.need-factory-setup
+        source /etc/profile.d/btcpay-env.sh
+        docker-compose -f $BTCPAY_DOCKER_COMPOSE down -t 20 # calling btcpay-down.sh hangs
+        docker volume ls -q | while read -r line ; do
+            if [ "$line" != "generated_bitcoin_datadir" ]; then
+                docker volume rm -f "$line"
+            fi
+        done
+        rm -rf "$MOUNT_DIR/generated_bitcoin_datadir/_data/debug.log"
+        echo "Deleted all volumes that are not related to bitcoin"
+        rm -rf $BTCPAY_HOST_SSHKEYFILE
+        authorized_keys="$(head -n -2 "/root/.ssh/authorized_keys")"
+        echo "Deleted BTCPay SSH key to access SSH host"
+        echo "$authorized_keys" > /root/.ssh/authorized_keys
+        rm -f /etc/ssh/ssh_host*
+        echo "Deleted SSH host keys"
+    fi
+    echo "255" > $white_led
+    echo "0" > $red_led
+    exit 0
+else
+    echo "0" > $white_led
+    echo "255" > $red_led
+    echo -e "[ \e[31mFailed\e[0m ] Some tests did not passed in less than $timeout seconds"
+    exit 1
+fi
