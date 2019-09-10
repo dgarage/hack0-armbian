d7827b569d10a1b45df2b190525f94bf64a04058
nicolas.dorier <nicolas.dorier@gmail.com>
2019-09-10 18:54:48 +0900

    Create btcpay keypair at the beginning of init

--- userpatches/overlay/btcpay-init.sh
+++ userpatches/overlay/btcpay-init.sh
@@ -17,6 +17,24 @@ fi
 
 cd /root
 
+if $SETUP_MODE || ! [ -f "$BTCPAY_HOST_SSHKEYFILE" ]; then
+    rm -rf "$BTCPAY_HOST_SSHKEYFILE"
+    echo "Creating BTCPay server key pair"
+    ssh-keygen -t rsa -f "$BTCPAY_HOST_SSHKEYFILE" -q -P "" -m PEM
+    echo -e "[ \e[32mOK\e[0m ] BTCPay Server SSH keypair created"
+else
+    echo -e "[ \e[32mOK\e[0m ] Do not create BTCPay Server SSH keys: $BTCPAY_HOST_SSHKEYFILE already exists"
+fi
+
+authorized_keys_file="/root/.ssh/authorized_keys"
+if grep -qF "Key used by BTCPay Server" "$authorized_keys_file"; then
+    echo -e "[ \e[32mOK\e[0m ] Do not add BTCPayServer key file to $authorized_keys_file: Already added"
+else
+    echo "# Key used by BTCPay Server" >> "$authorized_keys_file"
+    cat /root/.ssh/id_rsa_btcpay.pub >> "$authorized_keys_file"
+    echo -e "[ \e[32mOK\e[0m ] BTCPayServer key file added to $authorized_keys_file"
+fi
+
 $SETUP_MODE && rm -rf /root/.not_logged_in_yet
 if $SETUP_MODE && [ -f "docker-images.tar" ]; then
     echo "Loading docker images..."
@@ -35,23 +53,6 @@ if $SETUP_MODE && [ -f utxo-snapshot-*.tar ]; then
     ./load-utxo-set.sh $SNAPSHOT_TAR
     popd
     echo -e "[ \e[32mOK\e[0m ] UTXO Set preloaded."
-fi
-
-if ! [ -f "$BTCPAY_HOST_SSHKEYFILE" ]; then
-    echo "Creating BTCPay server key pair"
-    ssh-keygen -t rsa -f "$BTCPAY_HOST_SSHKEYFILE" -q -P "" -m PEM
-    echo -e "[ \e[32mOK\e[0m ] BTCPay Server SSH keypair created"
-else
-    echo -e "[ \e[32mOK\e[0m ] Do not create BTCPay Server SSH keys: $BTCPAY_HOST_SSHKEYFILE already exists"
-fi
-
-authorized_keys_file="/root/.ssh/authorized_keys"
-if grep -qF "Key used by BTCPay Server" "$authorized_keys_file"; then
-    echo -e "[ \e[32mOK\e[0m ] Do not add BTCPayServer key file to $authorized_keys_file: Already added"
-else
-    echo "# Key used by BTCPay Server" >> "$authorized_keys_file"
-    cat /root/.ssh/id_rsa_btcpay.pub >> "$authorized_keys_file"
-    echo -e "[ \e[32mOK\e[0m ] BTCPayServer key file added to $authorized_keys_file"
 fi
 
 if $SETUP_MODE; then
